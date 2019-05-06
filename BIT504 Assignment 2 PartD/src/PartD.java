2beaaf3fa91fba26eb5cc2acbcad5c065a442a99
Weltwandler <manuelgebhard@gmail.com>
2019-05-07 07:18:35 +1200

    Fourth Commit

--- BIT504 Assignment 2 PartD/src/PartD.java
+++ BIT504 Assignment 2 PartD/src/PartD.java
@@ -5,6 +5,7 @@ public class PartD {
 		System.out.println("Hi, my name is Manuel");
 		System.out.println("My favourite color is Teal");
 		System.out.println("My favourite mode of transportation is by motorbike");
+		System.out.println("The best day of the week is Saturday");
 
 	}
 
